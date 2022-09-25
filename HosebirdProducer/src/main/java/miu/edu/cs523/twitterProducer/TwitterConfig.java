fa182c062a1d99eadb2f3359f2ecb1f78cd42fd4
CitizenOneEt <87533861+CitizenOneEt@users.noreply.github.com>
2022-09-25 14:48:24 -0500

    Add files via upload

---
+++ HosebirdProducer/src/main/java/miu/edu/cs523/twitterProducer/TwitterConfig.java
@@ -0,0 +1,12 @@
+package miu.edu.cs523.twitterProducer;
+
+public abstract class TwitterConfig {
+
+	public static final String CONSUMER_KEY = "69wWJGAYF1QfoIUZPHIi3cOnJ";
+
+	public static final String CONSUMER_SECRET = "WT623qzypny11VdS1gNnsEPHADRqKbaXkxEKXDGMEeq1ASknJF";
+
+	public static final String TOKEN  = "1235044472024596480-tAeEjqy1GzKrrHhKHzOX9OE3O1MZ6N";
+
+	public static final String SECRET = "WMxQItZR71dX0N34uF699vmpM9ucX2GevkIaYgjaTkYr0";
+}
